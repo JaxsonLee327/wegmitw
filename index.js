460e529fedbd503935624d2573416ad135d0c296
Jaxson Lee <jaxsonlee327@gmail.com>
2022-07-06 06:06:39 +0900

    top market place api

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use('/home', require('./routes/api/home'));
 app.use('/a_overview', require('./routes/api/analytic_overview'));
 app.use('/a_leaderboard', require('./routes/api/analytic_leaderboard'));
-app.use('/a_newly_added', require('./routes/api/analytic_marketplaces'));
+app.use('/a_marketplace', require('./routes/api/analytic_marketplaces'));
 app.use('/a_top_coll', require('./routes/api/analytic_top_collections'));
 app.use('/a_top_mints', require('./routes/api/analytic_top_mints'));
 app.use('/a_top_sales', require('./routes/api/analytic_top_sales'));
--- routes/api/analytic_marketplaces.js
+++ routes/api/analytic_marketplaces.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 router.get('/', (req, res) => res.send('Hello, Home API'));
 
-router.get('/', (req, res) => {
-    res.json({});
+router.get('/get_top_markets', (req, res) => {
+    require("got")
+        .get("https://data.messari.io/api/v1/markets")
+        .json()
+        .then(response => {
+            console.log(response);
+            res.json(response);
+        });
 });
 
 
